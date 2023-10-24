02e275e776878e9f44b51a9b3b218dcf005314ee
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2023-10-24 18:44:23 +0100

    adds code for the increase and decrease in profit

--- index.js
+++ index.js
@@ -90,10 +90,8 @@ var finances = [
 console.log("Financial Analysis");
 
 // total number of months
-for (var i = 0; i < finances.length; i++) {
-  console.log("Total Months: " + finances.length);
-}
-
+for (var i = 0; i < finances.length; i++) {}
+console.log("Total Months: " + finances.length);
 // net total amount of profit/losses over the entire period
 // Create a variable for the sum and initialise it
 let sum = 0;
@@ -107,18 +105,77 @@ console.log("Total: $" + sum);
 
 // Average changes in Profit/Losses over the entire period
 let total = 0;
-for (var i = 1; i < finances.length; i++) {
-// tracking the total changes in profits from month to month
-  var financeChanges = finances[i][1];
-  var weekAheadChanges = finances[i - 1][1];
-  var currentWeekChanges = financeChanges - weekAheadChanges;
+for (let i = 1; i < finances.length; i++) {
+  // tracking the total changes in profits from month to month
+  let financeChanges = finances[i][1];
+  let weekAheadChanges = finances[i - 1][1];
+  let currentWeekChanges = financeChanges - weekAheadChanges;
   total += currentWeekChanges;
 }
 // console.log("averageFinances: " + total);
 
-// Average change throughout the months 
-var averageChange = total / (finances.length - 1);
+// Average change throughout the months
+let averageChange = total / (finances.length - 1);
 // print out the result and round it to 2 decimal places
 console.log("Average Change: " + averageChange.toFixed(2));
+
+// The greatest increase in profit (date and amount) over the entire period
+var change = 0;
+var least = ["", 9999999999999];
+var greatest = ["", 0];
+var net = 0;
+var netArray = [];
+var netChangeSum = 0;
+for (var i = 0; i < finances.length; i++) {
+  for (var j = 0; j < finances[i].length; j++) {
+    if (typeof finances[i][j] !== "string") {
+      total += finances[i][j];
+      change = finances[i][j] - net;
+      net = finances[i][j];
+      netArray.push(change);
+      // console.log(change)
+      // console.log(netArray)
+      // newArray = arr.sort(netArray);
+      if (change > greatest[1]) {
+        greatest = [finances[i][0], finances[i][1]];
+      }
+      if (change < least[1]) {
+        least = [finances[i][0], finances[i][1]];
+      }
+    }
+  }
+}
+largest(netArray);
+
+function largest(arr) {
+  let i;
+  // Initialize maximum element
+  let max = arr[0];
+  // Traverse array elements
+  // from second and compare
+  // every element with current max
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i];
+  }
+  return max;
+}
+
+function smallest(arr) {
+  let i;
+  // Initialize maximum element
+  let min = arr[0];
+  // Traverse array elements
+  // from second and compare
+  // every element with current max
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+  }
+  return min;
+}
+
+// Driver code
+let arr = netArray;
+console.log("Greatest increase in profits/losses: " + `$` + largest(arr));
+console.log("Greatest decrease in profits/losses: " + `$` + smallest(arr));
 
 
